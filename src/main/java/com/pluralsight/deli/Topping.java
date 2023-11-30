9839f3c999dec02d49a72682bc0a48b343c61d67
Jmartw <146871238+Jmartw@users.noreply.github.com>
2023-11-30 13:42:17 -0500

    added comments to toppings for developers to understand what code does

--- src/main/java/com/pluralsight/deli/Topping.java
+++ src/main/java/com/pluralsight/deli/Topping.java
@@ -1,16 +1,22 @@
 package com.pluralsight.deli;
 
-public abstract class Topping implements CostInterface{
-    private String name;
 
+public abstract class Topping implements CostInterface{
+
+    /**
+     * An abstract class representing a topping that can be added to a sandwich.
+     * Subclasses regular toppings and premium toppings are provided as specific implementations for the different type of toppings in the sandwich shop.
+     */
+    private String name;
+        //Constructors for toppings with given name
     public Topping(String name) {
         this.name = name;
     }
-
+        //Default constructor
     public Topping() {
-
+        //Default constructor left empty to be overwritten by child classes
     }
-
+        //Get name of topping
     public String getName() {
         return name;
     }
@@ -18,11 +24,12 @@ public abstract class Topping implements CostInterface{
     public void setName(String name) {
         this.name = name;
     }
-
+        //Get name and price of topping
     @Override
     public double getPrice() {
         return 0;
-    }
+        // The price of topping. subclasses will override method
+    }   // returns string of topic
 
     @Override
     public String toString() {
