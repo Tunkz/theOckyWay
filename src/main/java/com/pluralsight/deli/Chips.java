7833c7e6829ec1a8236b85866fbb2f02200a62ba
Jmartw <146871238+Jmartw@users.noreply.github.com>
2023-11-29 19:48:28 -0500

    comments for developers to understand code on lines 5,8,13,22,29 in chips class (#24)

--- src/main/java/com/pluralsight/deli/Chips.java
+++ src/main/java/com/pluralsight/deli/Chips.java
@@ -2,12 +2,15 @@ package com.pluralsight.deli;
 
 public class Chips implements CostInterface {
 
+    // Type of chips
    private String chipType;
 
+    // Constructor to initialize the chips with a specific chip type
     public Chips(String chipType) {
         this.chipType = chipType;
     }
 
+    // Getter and setter methods for chip type
     public String getChipType() {
         return chipType;
     }
@@ -16,11 +19,14 @@ public class Chips implements CostInterface {
         this.chipType = chipType;
     }
 
+    // Implementation of the CostInterface method to return the price of the chips
     @Override
+    // A fixed price for the chips
     public double getPrice() {
         return 1.5;
     }
 
+    // Override of the toString method to provide a string representation of the chips
     @Override
     public String toString() {
         return "Chips{" +
