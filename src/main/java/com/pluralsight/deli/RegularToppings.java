f28083cfba5e6a810d275f202336139f793b392b
Bubacari Tunkara <143526830+Tunkz@users.noreply.github.com>
2023-11-30 23:15:45 -0500

    Finilized addSandwich method (#38)

--- src/main/java/com/pluralsight/deli/RegularToppings.java
+++ src/main/java/com/pluralsight/deli/RegularToppings.java
@@ -1,8 +1,33 @@
 package com.pluralsight.deli;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegularToppings extends Topping{
+    private List<RegularToppings> regularToppings = new ArrayList<>();
     public RegularToppings(String name) {
         super(name);
+    }
+
+
+
+    public void setRegularToppings(List<RegularToppings> regularToppings) {
+        this.regularToppings = regularToppings;
+    }
+
+    public List<RegularToppings> getRegularToppings(){
+        List<RegularToppings> regularTopping= new ArrayList<>();
+        regularTopping.add(new RegularToppings("lettuce"));
+        regularTopping.add(new RegularToppings("peppers"));
+        regularTopping.add(new RegularToppings("onion"));
+        regularTopping.add(new RegularToppings("tomatoes"));
+        regularTopping.add(new RegularToppings("jalapenos"));
+        regularTopping.add(new RegularToppings("cucumbers"));
+        regularTopping.add(new RegularToppings("pickles"));
+        regularTopping.add(new RegularToppings("guacamole"));
+        regularTopping.add(new RegularToppings("mushrooms"));
+
+        return regularTopping;
     }
 
     @Override
