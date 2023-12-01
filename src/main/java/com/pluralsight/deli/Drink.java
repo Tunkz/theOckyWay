2395a6db82217e6796924f5912904c60cb447eda
Tunkz <Bubatunkz00@gmail.com>
2023-12-01 00:25:35 -0500

    finished adding items

--- src/main/java/com/pluralsight/deli/Drink.java
+++ src/main/java/com/pluralsight/deli/Drink.java
@@ -1,18 +1,28 @@
 package com.pluralsight.deli;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Drink implements CostInterface {
 
     private String size;
     private String flavor;
+    private List<Drink> drinks = new ArrayList<>();
 
     public Drink(String size, String flavor) {
         this.size = size;
         this.flavor = flavor;
     }
 
+    public Drink(String flavor) {
+        this.flavor = flavor;
+    }
+
+
     public String getSize() {
         return size;
     }
+
 
     public void setSize(String size) {
         this.size = size;
@@ -41,6 +51,16 @@ public class Drink implements CostInterface {
 
     @Override
     public String toString() {
-        return String.format("\nSize "+this.size+", Flavor "+this.flavor);
+        return String.format("\nSize " + this.size + ", Flavor " + this.flavor);
+    }
+
+    public List<Drink> getDrinks() {
+        List<Drink> drinks = new ArrayList<>();
+        drinks.add(new Drink("coke"));
+        drinks.add(new Drink("sprite"));
+        drinks.add(new Drink("fanta"));
+
+
+        return drinks;
     }
 }
